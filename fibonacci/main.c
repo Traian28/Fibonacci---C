18bca0511821c8634187e8a565ad4ddd03a36fb1
Traian28 <31419115+Traian28@users.noreply.github.com>
2019-04-16 19:20:27 -0300

    Add files via upload

---
+++ fibonacci/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n){
+	if(n<2)
+		return n;
+	else
+		return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main()
+{
+    char buffer;
+    FILE* archivo=fopen("C:\\Users\\Traian\\Desktop\\dato.txt","r");
+    fscanf(archivo,"%c", &buffer);
+    fclose(archivo);
+
+    int n=(buffer-'0');
+
+    int res=fibonacci(n);
+
+    archivo=fopen("C:\\Users\\Traian\\Desktop\\dato.txt","w");
+    fprintf(archivo,"%d",res);
+    fclose(archivo);
+
+    return 0;
+}
